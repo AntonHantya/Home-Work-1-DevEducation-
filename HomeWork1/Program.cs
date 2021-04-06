8837ab2a32b16d09af69c3691c75116bb16de142
DESKTOP-QIR84KR\Антон <aloe21252@gmail.com>
2021-04-06 15:33:17 +0300

    first commit

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task №1
+
+            /*Console.WriteLine("Enter the first number :");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number :");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double result;
+
+            if (b - a == 0)
+            {
+                Console.WriteLine("Please enter another number");
+            }
+            else
+            {
+                result = (5 * a + b * b) / (b - a);
+                Console.WriteLine($"Calculation result = {result}");
+            }*/
+
+
+
+
+            //Task №2
+
+            /*Console.WriteLine("Enter the number A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the number B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c;
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"a = {a}, b = {b}");*/
+
+
+
+            //Task №3
+
+            /*Console.WriteLine("Enter the number A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the number B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double division = a / b;
+            double percentOfDivision = a % b;
+
+            Console.WriteLine($"A / B = {division}");
+            Console.WriteLine($"A % B = {percentOfDivision}");*/
+
+
+
+            //Task №4
+
+            /*Console.WriteLine("Linear equation a * x + b = c");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the number A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the number B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the number C:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double result = (c - b) / a;
+
+            Console.WriteLine($"X = {result}");*/
+
+
+
+            //Task №5
+
+            /*Console.WriteLine("Enter the number X1:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the number Y1:");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the number X2:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the number Y2:");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double a = (y1 - y2) / (x1 -x2);
+            double b = y2-((y1-y2)/(x1-x2)) * x2;
+
+            Console.WriteLine($"Y = {a}x + {b}");*/
+
+        }
+    }
+}
